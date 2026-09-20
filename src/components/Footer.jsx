349a77c33350afbba154c53e87716f188ecab728
ARAVIND <garavindofficial@gmail.com>
2026-09-20 21:20:07 +0530

    max width removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-slate-900 text-gray-300">
 
-      <div className="max-w-7xl mx-auto px-6 py-16">
+      <div className=" mx-auto px-6 py-16">
 
         <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-4">
 
